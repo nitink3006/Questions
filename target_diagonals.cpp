bfbcd0f81457258b744bab200bca6bcfb39a99ad
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2022-10-18 22:47:40 +0530

    Create target_diagonals.cpp

---
+++ target_diagonals.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+const int N=100;
+
+//countNumberOfTimesTargetIsInDiagonal containing row, col
+int countNumberOfTimesTargetIsInDiagonal(int n, int A[][N], int row, int col, int target){
+	int ans=0;
+	int i,j;
+	for(int i=row,j=col;i<n&&j<n;i++,j++){
+	    if(A[i][j]==target){
+	        ans++;
+	    }
+	}
+	
+	for(int i=row,j=col;i>=0&&j<n;i--,j++){
+	    if(A[i][j]==target){
+	        ans++;
+	    }
+	}
+	
+	for(int i=row,j=col;i>=0&&j>=0;i++,j--){
+	    if(A[i][j]==target){
+	        ans++;
+	    }
+	}
+	
+	for(int i=row,j=col;i>n&&j<0;i++,j++){
+	    if(A[i][j]==target){
+	        ans++;
+	    }
+	}
+	
+	if(A[i][j]==target){
+	    ans=ans-3;
+	}
+	
+	return ans;
+	
+}
+
+
+int main() {
+	int n;
+	cin>>n;
+	int A[N][N];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cin>>A[i][j];
+		}
+	}
+	cout<<"Ans: "<<countNumberOfTimesTargetIsInDiagonal(n, A, 1, 2, 2)<<"\n";
+	return 0;
+}
+
